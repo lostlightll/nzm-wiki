9c96bcb4345dfa825595356380b020a9eedfd338
qiekn <qiekndev@gmail.com>
2026-02-11 02:56:01 +0800

    feat: 计算器添加过渡动画，支持拖动和改变窗口大小。添加了命令面板，ctrl/cmd + shift + p 打开

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Calculator } from "@/components/Calculator";
+import { GlobalTools } from "@/components/GlobalTools";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +35,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Calculator />
+        <GlobalTools />
       </body>
     </html>
   );
